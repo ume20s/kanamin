53f443f5837b67e046d913bf3f3476a9b5459140
ume20s <ume20s@gmail.com>
2024-02-24 11:37:51 +0900

    問題文をランダムに出題

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -21,8 +21,12 @@ public class GameDirector : MonoBehaviour
     GameObject additionalInfoText;                          // 追加情報テキスト
 
     // 各種変数
-    int stage;          // ステージ
-    int gameState;      // 状態遷移変数
+    int stage;              // ステージ
+    int quesCounter;        // 現在何問目？
+    int gameState;          // 状態遷移変数
+
+    // 各状態実行中フラグ
+    bool dispStageFlg;      // ステージアイキャッチ表示
 
     // Start is called before the first frame update
     void Start()
@@ -44,30 +48,41 @@ public class GameDirector : MonoBehaviour
 
         // 各種変数の初期化
         stage = 1;
+        quesCounter = 0;
         gameState = 0;
+
+        // 各状態実行中フラグは全部false
+        dispStageFlg = false;
+
+        // クイズ出題順のシャッフル
+        int n = 100;
+        while(n > 1) {
+            n--;
+            int k = Random.Range(0, 100);
+            int temp = Ques.Order[k];
+            Ques.Order[k] = Ques.Order[n];
+            Ques.Order[n] = temp;
+        }
 
         // 最初に余計なものを消しておく
         stageEyeCatchFrame.SetActive(false);
         stageNumberText.SetActive(false);
         additionalInfoFrame.SetActive(false);
         kanaminThinking.SetActive(false);
-
     }
 
     // Update is called once per frame
     void Update()
     {
         switch(gameState) {
+            // ステージアイキャッチの表示
             case 0:
-                // ステージアイキャッチの表示
                 StartCoroutine("DispStage");
-
-
-                // ゲーム実行中へ遷移
-                gameState++;
                 break;
             
+            // クイズ出題
             case 1:
+                AskQues();
                 break;
 
             default:
@@ -79,36 +94,55 @@ public class GameDirector : MonoBehaviour
     // ステージアイキャッチの表示
     IEnumerator DispStage()
     {
-        // アイキャッチ背景のトランスフォームコンポーネントの取得
-        Transform tf = stageEyeCatchFrame.GetComponent<Transform>();
+        if(!dispStageFlg) {
+            // ステージアイキャッチ中
+            dispStageFlg = true;
+            
+            // アイキャッチ背景のトランスフォームコンポーネントの取得
+            Transform tf = stageEyeCatchFrame.GetComponent<Transform>();
 
-        // ステージ文字列をセット
-        stageNumberText.GetComponent<Text>().text = "STAGE: " + stage.ToString();
+            // ステージ文字列をセット
+            stageNumberText.GetComponent<Text>().text = "STAGE: " + stage.ToString();
 
-        // アイキャッチ背景の高さをゼロにしてから表示
-        tf.transform.localScale = new Vector3(1, 0, 1);
-        stageEyeCatchFrame.SetActive(true);
+            // アイキャッチ背景の高さをゼロにしてから表示
+            tf.transform.localScale = new Vector3(1, 0, 1);
+            stageEyeCatchFrame.SetActive(true);
 
-        // アイキャッチ背景をジワジワ大きくする
-        for(int i=0; i<10; i++) {
-            tf.transform.localScale = new Vector3(1, (float)(i) / 10.0f, 1);
-            yield return new WaitForSeconds(0.02f);
+            // アイキャッチ背景をジワジワ大きくする
+            for(int i=0; i<10; i++) {
+                tf.transform.localScale = new Vector3(1, (float)(i) / 10.0f, 1);
+                yield return new WaitForSeconds(0.02f);
+            }
+
+            // ステージ番号テキストを表示して2.5秒待つ
+            stageNumberText.SetActive(true);
+            audioSource.PlayOneShot(vStageStart);
+            yield return new WaitForSeconds(2.5f);
+
+            // ステージアイキャッチを非表示
+            stageEyeCatchFrame.SetActive(false);
+            stageNumberText.SetActive(false);
+
+            // かなみん表示
+            kanaminThinking.SetActive(true);
+
+            // ＢＧＭ開始
+            audioSource.Play();
+
+            // クイズ出題フェーズへ遷移
+            gameState++;
+
+            // ステージアイキャッチ終了
+            dispStageFlg = false;
         }
-
-        // ステージ番号テキストを表示して2.5秒待つ
-        stageNumberText.SetActive(true);
-        audioSource.PlayOneShot(vStageStart);
-        yield return new WaitForSeconds(2.5f);
-
-        // ステージアイキャッチを非表示
-        stageEyeCatchFrame.SetActive(false);
-        stageNumberText.SetActive(false);
-
-        // かなみん表示
-        kanaminThinking.SetActive(true);
-
-        // ＢＧＭ開始
-        audioSource.Play();
     }
+
+    // クイズ出題
+    private void AskQues()
+    {
+        questionText.GetComponent<Text>().text = Ques.Q[Ques.Order[quesCounter]];
+
+    }
+
 
 }
--- kanaminAndroid_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
+++ kanaminAndroid_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -18,56 +18,59 @@ extern void GameDirector_Start_m02BE40249C44E2ECC4035D59D4A70F6678A41E9C (void);
 extern void GameDirector_Update_m3D758017B69C5F1B1E381BDED2E19E77A17298A5 (void);
 // 0x00000003 System.Collections.IEnumerator GameDirector::DispStage()
 extern void GameDirector_DispStage_m8DB17BF431A1A541F0EC4DD6BB0754CC4CE4F242 (void);
-// 0x00000004 System.Void GameDirector::.ctor()
+// 0x00000004 System.Void GameDirector::AskQues()
+extern void GameDirector_AskQues_m4404C0D438C4669863FA4E6EBAAC8295481B0FD6 (void);
+// 0x00000005 System.Void GameDirector::.ctor()
 extern void GameDirector__ctor_m11BFC3A4A757A802BDD31DFC5E5F1D19E54DF1DD (void);
-// 0x00000005 System.Void GameDirector/<DispStage>d__14::.ctor(System.Int32)
-extern void U3CDispStageU3Ed__14__ctor_m3F946E53D58F1459AC51AE991AD9B46516697257 (void);
-// 0x00000006 System.Void GameDirector/<DispStage>d__14::System.IDisposable.Dispose()
-extern void U3CDispStageU3Ed__14_System_IDisposable_Dispose_mB34817E2D64D92B68BF2F0B02D204A1D81F9C182 (void);
-// 0x00000007 System.Boolean GameDirector/<DispStage>d__14::MoveNext()
-extern void U3CDispStageU3Ed__14_MoveNext_m0233C4D481B4C249E166B0A6F4265EE78E57C80B (void);
-// 0x00000008 System.Object GameDirector/<DispStage>d__14::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
-extern void U3CDispStageU3Ed__14_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mDD6DBA325F3B2CD3180CF65447DB3E49F68F40B9 (void);
-// 0x00000009 System.Void GameDirector/<DispStage>d__14::System.Collections.IEnumerator.Reset()
-extern void U3CDispStageU3Ed__14_System_Collections_IEnumerator_Reset_m1205925B375408FDACADADED726AB9DD9AB81454 (void);
-// 0x0000000A System.Object GameDirector/<DispStage>d__14::System.Collections.IEnumerator.get_Current()
-extern void U3CDispStageU3Ed__14_System_Collections_IEnumerator_get_Current_mF00AF68DCB49CEDE7D6B103FEBBAC4191D9F0C97 (void);
-// 0x0000000B System.Void taptoStart::Start()
+// 0x00000006 System.Void GameDirector/<DispStage>d__16::.ctor(System.Int32)
+extern void U3CDispStageU3Ed__16__ctor_mA4FB46557A8C417B12778F4557737A80E5D8B9C2 (void);
+// 0x00000007 System.Void GameDirector/<DispStage>d__16::System.IDisposable.Dispose()
+extern void U3CDispStageU3Ed__16_System_IDisposable_Dispose_m784EE6D4B7EE1058ACBB80BD9D0EBCAA1D11AAA1 (void);
+// 0x00000008 System.Boolean GameDirector/<DispStage>d__16::MoveNext()
+extern void U3CDispStageU3Ed__16_MoveNext_m70AD26F23B18CB7707B855154041804D25943C2C (void);
+// 0x00000009 System.Object GameDirector/<DispStage>d__16::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CDispStageU3Ed__16_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCCD6DA00EFDC51747791D7795882E5AD4DED8AA1 (void);
+// 0x0000000A System.Void GameDirector/<DispStage>d__16::System.Collections.IEnumerator.Reset()
+extern void U3CDispStageU3Ed__16_System_Collections_IEnumerator_Reset_m803E54E5A232E89C952047F40F037078AC9D9422 (void);
+// 0x0000000B System.Object GameDirector/<DispStage>d__16::System.Collections.IEnumerator.get_Current()
+extern void U3CDispStageU3Ed__16_System_Collections_IEnumerator_get_Current_m66A43DB6E43ED7C96E9B1CEC268B5C6C066A92BF (void);
+// 0x0000000C System.Void taptoStart::Start()
 extern void taptoStart_Start_mB0353D6D50760FF5DE65038C6F134F0C67A090A4 (void);
-// 0x0000000C System.Void taptoStart::onClick()
+// 0x0000000D System.Void taptoStart::onClick()
 extern void taptoStart_onClick_m9A0B7565F878F6B5F7FAB434FD0F25BDAA5CD741 (void);
-// 0x0000000D System.Collections.IEnumerator taptoStart::waitOneSecAndStart()
+// 0x0000000E System.Collections.IEnumerator taptoStart::waitOneSecAndStart()
 extern void taptoStart_waitOneSecAndStart_m970EE5A36918A913DE51400FB9681142E9878781 (void);
-// 0x0000000E System.Void taptoStart::.ctor()
+// 0x0000000F System.Void taptoStart::.ctor()
 extern void taptoStart__ctor_m0805AC779FE6764577415754D601B1901DFCE186 (void);
-// 0x0000000F System.Void taptoStart/<waitOneSecAndStart>d__9::.ctor(System.Int32)
+// 0x00000010 System.Void taptoStart/<waitOneSecAndStart>d__9::.ctor(System.Int32)
 extern void U3CwaitOneSecAndStartU3Ed__9__ctor_m203A75574EBBD6FB050C01DE8BBA29CA7D5B2465 (void);
-// 0x00000010 System.Void taptoStart/<waitOneSecAndStart>d__9::System.IDisposable.Dispose()
+// 0x00000011 System.Void taptoStart/<waitOneSecAndStart>d__9::System.IDisposable.Dispose()
 extern void U3CwaitOneSecAndStartU3Ed__9_System_IDisposable_Dispose_m025923A1D06927D399A941412E8A43A841507A1F (void);
-// 0x00000011 System.Boolean taptoStart/<waitOneSecAndStart>d__9::MoveNext()
+// 0x00000012 System.Boolean taptoStart/<waitOneSecAndStart>d__9::MoveNext()
 extern void U3CwaitOneSecAndStartU3Ed__9_MoveNext_m2887926D163694FA85EED6B9B4F353448C8F1ECC (void);
-// 0x00000012 System.Object taptoStart/<waitOneSecAndStart>d__9::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+// 0x00000013 System.Object taptoStart/<waitOneSecAndStart>d__9::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
 extern void U3CwaitOneSecAndStartU3Ed__9_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m1002A565D871AFEB52C10D84560594EC5335543F (void);
-// 0x00000013 System.Void taptoStart/<waitOneSecAndStart>d__9::System.Collections.IEnumerator.Reset()
+// 0x00000014 System.Void taptoStart/<waitOneSecAndStart>d__9::System.Collections.IEnumerator.Reset()
 extern void U3CwaitOneSecAndStartU3Ed__9_System_Collections_IEnumerator_Reset_mF68B876EB3F7DF434F590E49BA334D1EF6FE375E (void);
-// 0x00000014 System.Object taptoStart/<waitOneSecAndStart>d__9::System.Collections.IEnumerator.get_Current()
+// 0x00000015 System.Object taptoStart/<waitOneSecAndStart>d__9::System.Collections.IEnumerator.get_Current()
 extern void U3CwaitOneSecAndStartU3Ed__9_System_Collections_IEnumerator_get_Current_mCED8B3025D33B5EBA97B8BE159796EF66F45E12C (void);
-// 0x00000015 System.Void Ques::.ctor()
+// 0x00000016 System.Void Ques::.ctor()
 extern void Ques__ctor_m7CE8713D4FE6A5A6F1C2CE5B141D6D4B025061C7 (void);
-// 0x00000016 System.Void Ques::.cctor()
+// 0x00000017 System.Void Ques::.cctor()
 extern void Ques__cctor_m0DF43951936357FCFB6884458C55190612E10BFE (void);
-static Il2CppMethodPointer s_methodPointers[22] = 
+static Il2CppMethodPointer s_methodPointers[23] = 
 {
 	GameDirector_Start_m02BE40249C44E2ECC4035D59D4A70F6678A41E9C,
 	GameDirector_Update_m3D758017B69C5F1B1E381BDED2E19E77A17298A5,
 	GameDirector_DispStage_m8DB17BF431A1A541F0EC4DD6BB0754CC4CE4F242,
+	GameDirector_AskQues_m4404C0D438C4669863FA4E6EBAAC8295481B0FD6,
 	GameDirector__ctor_m11BFC3A4A757A802BDD31DFC5E5F1D19E54DF1DD,
-	U3CDispStageU3Ed__14__ctor_m3F946E53D58F1459AC51AE991AD9B46516697257,
-	U3CDispStageU3Ed__14_System_IDisposable_Dispose_mB34817E2D64D92B68BF2F0B02D204A1D81F9C182,
-	U3CDispStageU3Ed__14_MoveNext_m0233C4D481B4C249E166B0A6F4265EE78E57C80B,
-	U3CDispStageU3Ed__14_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mDD6DBA325F3B2CD3180CF65447DB3E49F68F40B9,
-	U3CDispStageU3Ed__14_System_Collections_IEnumerator_Reset_m1205925B375408FDACADADED726AB9DD9AB81454,
-	U3CDispStageU3Ed__14_System_Collections_IEnumerator_get_Current_mF00AF68DCB49CEDE7D6B103FEBBAC4191D9F0C97,
+	U3CDispStageU3Ed__16__ctor_mA4FB46557A8C417B12778F4557737A80E5D8B9C2,
+	U3CDispStageU3Ed__16_System_IDisposable_Dispose_m784EE6D4B7EE1058ACBB80BD9D0EBCAA1D11AAA1,
+	U3CDispStageU3Ed__16_MoveNext_m70AD26F23B18CB7707B855154041804D25943C2C,
+	U3CDispStageU3Ed__16_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCCD6DA00EFDC51747791D7795882E5AD4DED8AA1,
+	U3CDispStageU3Ed__16_System_Collections_IEnumerator_Reset_m803E54E5A232E89C952047F40F037078AC9D9422,
+	U3CDispStageU3Ed__16_System_Collections_IEnumerator_get_Current_m66A43DB6E43ED7C96E9B1CEC268B5C6C066A92BF,
 	taptoStart_Start_mB0353D6D50760FF5DE65038C6F134F0C67A090A4,
 	taptoStart_onClick_m9A0B7565F878F6B5F7FAB434FD0F25BDAA5CD741,
 	taptoStart_waitOneSecAndStart_m970EE5A36918A913DE51400FB9681142E9878781,
@@ -81,11 +84,12 @@ static Il2CppMethodPointer s_methodPointers[22] =
 	Ques__ctor_m7CE8713D4FE6A5A6F1C2CE5B141D6D4B025061C7,
 	Ques__cctor_m0DF43951936357FCFB6884458C55190612E10BFE,
 };
-static const int32_t s_InvokerIndices[22] = 
+static const int32_t s_InvokerIndices[23] = 
 {
 	3206,
 	3206,
 	3125,
+	3206,
 	3206,
 	2607,
 	3206,
@@ -110,7 +114,7 @@ IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
 const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
 {
 	"Assembly-CSharp.dll",
-	22,
+	23,
 	s_methodPointers,
 	0,
 	NULL,
