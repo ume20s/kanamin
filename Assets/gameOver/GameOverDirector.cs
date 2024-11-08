edade796568b9afd7d5d39c7d801951e0cb60406
ume20s <ume20s@gmail.com>
2024-11-09 08:21:56 +0900

    ゲームオーバーアセットの追加

--- Assets/gameOver/GameOverDirector.cs
+++ Assets/gameOver/GameOverDirector.cs
@@ -21,36 +21,29 @@ public class GameOverDirector : MonoBehaviour
         StartCoroutine("GameOver");
     }
 
-    // ステージアイキャッチの表示
+    // ゲームオーバーエフェクト
     IEnumerator GameOver()
     {
-        // ざんねん効果音
-        audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(seGameOver);
-
         // ゲームオブジェクトの取得
         kanaminZannen = GameObject.Find("kanaminZannen");           // かなみん残念グラフィック
 
         // かなみんのトランスフォームコンポーネントの取得
         Transform tf = kanaminZannen.GetComponent<Transform>();
 
-        // かなみんをジワジワちいさくする
+        // ざんねん効果音
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(seGameOver);
+
+        // かなみん中央へ移動しつつジワジワちいさくする
         for(int i=20; i>10; i--) {
             tf.transform.localScale = new Vector3(1.8f * (float)(i) / 20.0f, 1.8f * (float)(i) / 20.0f, 1);
             yield return new WaitForSeconds(0.1f);
         }
 
-
-        // かなみん小さくなる
-
-
-
-
         // ざんねん音声
         audioSource.PlayOneShot(vGameOver);
 
     }
-
 
     // タップしたら
     public void onClick()
