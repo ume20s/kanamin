e90c57cb4f53cad077603d49c6dcf3feb9f452e4
ume20s <ume20s@gmail.com>
2024-11-13 20:41:12 +0900

    ゲームクリア素材追加

---
+++ Assets/GameClear/GameClearDirector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClearDirector : MonoBehaviour
+{
+    // 音声まわりのあれこれ
+    AudioSource audioSource;
+    public AudioClip seGameClear;               // ゲームクリア効果音
+    public AudioClip vGameClear;                // ゲームクリア音声
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
