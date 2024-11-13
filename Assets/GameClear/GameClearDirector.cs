fbd7364c7ce4cc6184f459e35049f2e3e5ea9532
ume20s <ume20s@gmail.com>
2024-11-13 22:40:04 +0900

    ゲームクリア素材の追加

--- Assets/GameClear/GameClearDirector.cs
+++ Assets/GameClear/GameClearDirector.cs
@@ -10,15 +10,36 @@ public class GameClearDirector : MonoBehaviour
     public AudioClip seGameClear;               // ゲームクリア効果音
     public AudioClip vGameClear;                // ゲームクリア音声
 
+    // ゲームオブジェクト
+    GameObject kanaminHyoshojo;                 // 表彰状グラフィック
+    GameObject tappingArea;                     // タップ領域
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ゲームクリアエフェクト
+        StartCoroutine("GameClearEffect");
     }
 
-    // Update is called once per frame
-    void Update()
+    // ゲームクリアエフェクト
+    IEnumerator GameClearEffect() 
     {
-        
+        // ゲームオブジェクトの取得
+        kanaminHyoshojo = GameObject.Find("hyoshojo");              // 表彰状グラフィック
+        tappingArea = GameObject.Find("TappingArea");               // タップ領域
+
+        // ゲームオブジェクトのトランスフォームコンポーネントの取得
+        Transform hyoshojoTf = kanaminHyoshojo.GetComponent<Transform>();
+
+        // タップ無効
+        tappingArea.SetActive(false);
+
+        // ゲームクリア効果音
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(seGameClear);
+
+        yield return new WaitForSeconds(0.01f);
+
     }
+
 }
